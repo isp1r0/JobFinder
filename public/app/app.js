b31aaaaa7eb165fcc72f589b1722fb0d1f5b068e
roysbailey <roysbailey@silvertouch.co.uk>
2014-11-04 08:44:15 +0000

    Fixed model error

--- public/app/app.js
+++ public/app/app.js
@@ -6,11 +6,7 @@ app = angular.module('app', ['ngResource']);
 
 angular.module('app').controller('testCtrl', function($scope, $resource, jobs){
     $scope.jobs = $resource('/api/jobs').query();
-    //jobs.save({title: "test title", description: "test description"});
-    //$scope.jobs = [
-    //    {title: 'Sales Person', description: 'A cool job with loadsa money!'},
-    //    {title: 'Technical Architect',description: 'Designing stuff that works!  How cool!'
-    //    }];
+
     $scope.submit=function(){
         var newJob = {title: $scope.title, description: $scope.description};
         jobs.save(newJob);
